e70f2efba138b4bb208cf0980a5abe25fcec4a58
zoomjuice <itsjunkmaill@gmail.com>
2021-03-16 16:06:12 -0400

    Complete playable prototype

---
+++ hanoi.py
@@ -0,0 +1,47 @@
+from math import inf
+from os import system, name
+
+tower_1 = [inf, 3, 2, 1]
+tower_2 = [inf]
+tower_3 = [inf]
+filled = [inf, 3, 2, 1]
+towers = [None, tower_1, tower_2, tower_3]
+
+
+def cls():
+    system('cls' if name == 'nt' else 'clear')
+    return
+
+
+def move_disk(from_tower, to_tower):
+    if from_tower[-1] == inf:
+        raise ValueError('This tower has no disks!')
+    elif from_tower[-1] > to_tower[-1]:
+        raise ValueError('You can\'t place a larger disk on top of a smaller disk')
+    else:
+        to_tower.append(from_tower.pop())
+
+
+while True:
+    print('Tower 1:', tower_1[1:])
+    print('Tower 2:', tower_2[1:])
+    print('Tower 3:', tower_3[1:])
+
+    src = int(input('Move from which tower? (1, 2, 3): '))
+    dst = int(input('Move to which tower? (1, 2, 3): '))
+
+    try:
+        move_disk(towers[src], towers[dst])
+        cls()
+    except ValueError as e:
+        cls()
+        print(str(e))
+        continue
+    except IndexError as e:
+        cls()
+        print('Invalid tower')
+        continue
+
+    if tower_3 == filled:
+        print('You win!')
+        break

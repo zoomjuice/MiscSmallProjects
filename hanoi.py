aa81eca62b9fb3357d98744e61f893b9662b4075
zoomjuice <itsjunkmaill@gmail.com>
2021-03-18 08:11:19 -0400

    Make initial tower size selection more robust

--- hanoi.py
+++ hanoi.py
@@ -1,13 +1,6 @@
 from math import inf
 from os import system, name
 
-tower_size = int(input('How many disks would you like to play with?: '))
-tower_1 = [inf] + [i for i in range(tower_size, 0, -1)]
-tower_2 = [inf]
-tower_3 = [inf]
-filled = [inf] + [i for i in range(tower_size, 0, -1)]
-towers = [None, tower_1, tower_2, tower_3]
-
 
 def cls():
     system('cls' if name == 'nt' else 'clear')
@@ -23,15 +16,44 @@ def move_disk(from_tower, to_tower):
         to_tower.append(from_tower.pop())
 
 
+welcome_string = """
+    Towers of Hanoi rules:
+        -
+    """
+
+while True:
+    tower_size = None
+    try:
+        tower_size = int(input('How many disks would you like to play with?: '))
+    except ValueError:
+        print('Sorry, I didn\'t understand that. Are you sure you entered a whole number? Please try again.')
+        continue
+
+    if tower_size <= 1:
+        print('You must have at least 3 disks in your tower. Please choose a new number.')
+        continue
+    elif tower_size > 10:
+        print('Your game would take at least {m} moves to solve, try a smaller tower.'.format(m=(2**tower_size - 1)))
+        continue
+    else:
+        break
+
+tower_1 = [inf] + [i for i in range(tower_size, 0, -1)]
+tower_2 = [inf]
+tower_3 = [inf]
+filled = [inf] + [i for i in range(tower_size, 0, -1)]
+towers = [None, tower_1, tower_2, tower_3]
+
+
 def build_towers():
     t1_pad = tower_1[1:] + [0] * (len(filled) - len(tower_1) + 1)
     t2_pad = tower_2[1:] + [0] * (len(filled) - len(tower_2) + 1)
     t3_pad = tower_3[1:] + [0] * (len(filled) - len(tower_3) + 1)
     tower_art = ''
     for i in range(1, len(filled) + 1):
-        t1_art = ' ' * (len(filled) - t1_pad[-i]) + '■' * t1_pad[-i] + '|' + '■' * t1_pad[-i] + ' ' * (len(filled) - t1_pad[-i])
-        t2_art = ' ' * (len(filled) - t2_pad[-i]) + '■' * t2_pad[-i] + '|' + '■' * t2_pad[-i] + ' ' * (len(filled) - t2_pad[-i])
-        t3_art = ' ' * (len(filled) - t3_pad[-i]) + '■' * t3_pad[-i] + '|' + '■' * t3_pad[-i] + ' ' * (len(filled) - t3_pad[-i])
+        t1_art = ' ' * (len(filled) - t1_pad[-i]) + '█' * t1_pad[-i] + '|' + '█' * t1_pad[-i] + ' ' * (len(filled) - t1_pad[-i])
+        t2_art = ' ' * (len(filled) - t2_pad[-i]) + '█' * t2_pad[-i] + '|' + '█' * t2_pad[-i] + ' ' * (len(filled) - t2_pad[-i])
+        t3_art = ' ' * (len(filled) - t3_pad[-i]) + '█' * t3_pad[-i] + '|' + '█' * t3_pad[-i] + ' ' * (len(filled) - t3_pad[-i])
         tower_art += t1_art + t2_art + t3_art + '\n'
     print(tower_art)
 

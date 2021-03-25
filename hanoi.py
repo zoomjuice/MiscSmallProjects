75dad669925829c9bb0d173a979b17f5957e0f03
zoomjuice <itsjunkmaill@gmail.com>
2021-03-25 10:41:14 -0400

    Add comments to code

--- hanoi.py
+++ hanoi.py
@@ -1,15 +1,24 @@
+"""
+TODO:
+    - Refactor build_towers()
+    - Add art and screen consistency to gameplay portion
+    - Show users their number of moves vs. perfect play
+    - Don't automatically exit on completion
+    - Add support for INI files?
+"""
+
 from os import system, name
 
 
+# Clear screen
 def cls():
     system('cls' if name == 'nt' else 'clear')
     return
 
 
+# Verify validity of and perform user's chosen move
 def move_disk(from_tower, to_tower):
-    print(from_tower)
-    print(to_tower)
-    if len(from_tower) == 0:
+    if len(from_tower) == 0:  # Make sure user isn't moving from an empty tower
         raise ValueError('This tower has no disks!')
     elif len(to_tower) != 0 and from_tower[-1] > to_tower[-1]:
         raise ValueError('You can\'t place a larger disk on top of a smaller disk')
@@ -17,28 +26,37 @@ def move_disk(from_tower, to_tower):
         to_tower.append(from_tower.pop())
 
 
+# Repeatedly ask user to select tower until a valid selection is made
 def select_tower(message):
     while True:
         ret_val = input(message)
         if ret_val in towers.keys():
             return ret_val
         else:
+            print('Invalid tower. Please try again.')
             continue
 
 
+# Calculate padding and disk sizes, then draw towers
 def build_towers():
     tower_art = ''
+
+    # Zero-pad towers to uniform length of completed tower, plus one empty space on top for the center rod
     t1_pad = tower_1.copy() + [0] * (len(filled) - len(tower_1) + 1)
     t2_pad = tower_2.copy() + [0] * (len(filled) - len(tower_2) + 1)
     t3_pad = tower_3.copy() + [0] * (len(filled) - len(tower_3) + 1)
 
+    # Add 1 to range since it stops before the end number, add another 1 for an empty space on top of the tower
     for i in range(1, len(filled) + 2):
-        t1_leftside = str(' ' * (len(filled) - t1_pad[-i]) + '█' * t1_pad[-i])
-        t2_leftside = str(' ' * (len(filled) - t2_pad[-i]) + '█' * t2_pad[-i])
-        t3_leftside = str(' ' * (len(filled) - t3_pad[-i]) + '█' * t3_pad[-i])
-        t1_art = t1_leftside + '|' + t1_leftside[::-1]
-        t2_art = t2_leftside + '|' + t2_leftside[::-1]
-        t3_art = t3_leftside + '|' + t3_leftside[::-1]
+        # Build left side of each tower's current layer
+        t1_left_side = str(' ' * (len(filled) - t1_pad[-i]) + '█' * t1_pad[-i])
+        t2_left_side = str(' ' * (len(filled) - t2_pad[-i]) + '█' * t2_pad[-i])
+        t3_left_side = str(' ' * (len(filled) - t3_pad[-i]) + '█' * t3_pad[-i])
+        # Append center rod followed by the reverse image of the left side to make the right
+        t1_art = t1_left_side + '|' + t1_left_side[::-1]
+        t2_art = t2_left_side + '|' + t2_left_side[::-1]
+        t3_art = t3_left_side + '|' + t3_left_side[::-1]
+        # Combine each tower segment into a layer and add it to the final string
         tower_art += t1_art + t2_art + t3_art + '\n'
     print(tower_art)
 
@@ -70,17 +88,20 @@ intro_string = """
 intro_status = 'Let\'s begin.'
 
 while True:
+    # Maintain consistent screen display while user is selecting tower sizes
     cls()
     print(intro_string)
     print(intro_status + '\n')
     tower_size = None
+
     try:
         tower_size = int(input('How many disks would you like to play with?: '))
     except ValueError:
         intro_status = 'Sorry, I didn\'t understand that. Please try again.'
         continue
 
-    if tower_size <= 1:
+    # Make sure the user selects a reasonably sized tower
+    if tower_size <= 2:
         intro_status = 'You must have at least 3 disks in your tower. Please choose a new number.'
         continue
     elif tower_size > 100:
@@ -97,19 +118,26 @@ while True:
     else:
         break
 
+# Initialize towers with disks ordered from largest to smallest based on user's selected tower size
 tower_1 = [i for i in range(tower_size, 0, -1)]
 tower_2 = []
 tower_3 = []
-filled = [i for i in range(tower_size, 0, -1)]
+
+# Generate completed tower to check against for win condition
+filled = tower_1.copy()
+
+# Tower selection during gameplay
 towers = {'1': tower_1, '2': tower_2, '3': tower_3}
 
 while True:
+    cls()
     build_towers()
 
-    # make a tower selection function
+    # Get move from player
     src = select_tower('Move from which tower? (1, 2, 3): ')
     dst = select_tower('Move to which tower? (1, 2, 3): ')
 
+    # Execute player's move
     try:
         move_disk(towers.get(src), towers.get(dst))
         cls()
@@ -118,6 +146,7 @@ while True:
         print(str(e))
         continue
 
+    # Check to see if either of the initially empty towers now has a full tower
     if tower_3 == filled or tower_2 == filled:
         print('You win!')
         break
